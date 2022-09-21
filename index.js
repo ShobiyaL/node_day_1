1abf43ae663afbc246b3ce259de699bd9f6e9e65
sofy sofya <sofy7468612@gmail.com>
2022-09-21 15:13:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const date = require('date-and-time')
+const fs = require("fs")
+
+//Get current date and Time.
+const now = new Date();
+const value = date.format(now, 'DD-MM-YYYY,HH-mm-ss')
+
+
+//Make a text file with current date and time.
+fs.writeFile("date-time.txt", `${value}`, function (err) {
+    if (err) throw err
+    
+})
+
+//Read a File Directory.
+    fs.readFile("date-time.txt","utf-8",function(err,data){
+    if(err) throw err;
+    console.log(data);
+})
